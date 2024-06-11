7045e4c8843b20c74a568ada38156f3117c4a747
Olena Zdanovska <freshjs.ftm@gmail.com>
2024-06-11 10:17:48 +0300

    Add boolean props without value

--- src/App.js
+++ src/App.js
@@ -1,13 +1,12 @@
 import './App.css';
 import Ciao from './components/Ciao';
 
-
 function App() {
   return (
     <>
-      <Ciao name="Fred" age={45} isMale={true}/>
-      <Ciao name="Bred" age={51} isMale={true}/>
-      <Ciao name="Anna" age={24} isMale={false}/>
+      <Ciao name="Fred" age={45} isMale />
+      <Ciao name="Bred" age={51} isMale />
+      <Ciao name="Anna" age={24} />
     </>
   );
 }
