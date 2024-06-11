9cbb26c7b8ad537a0ab691de57b21c3b7eb9de02
Olena Zdanovska <freshjs.ftm@gmail.com>
2024-06-11 10:30:15 +0300

    Add state to Ciao

--- src/components/Ciao/index.jsx
+++ src/components/Ciao/index.jsx
@@ -1,12 +1,20 @@
 import React, { Component } from 'react';
 
 class Ciao extends Component {
+  constructor() {
+    super();
+    this.state = {
+      isHi: true,
+      isSelect: false,
+    };
+  }
   render() {
+    const { isHi } = this.state;
     const { name, age, isMale } = this.props;
     return (
       <article>
         <h2>
-          Hi, {name}! ({age})
+          {isHi?'Hi':'Bye'}, {name}! ({age})
         </h2>
         <p>gender: {isMale ? 'male' : 'female'} </p>
       </article>
