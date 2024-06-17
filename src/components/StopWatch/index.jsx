713176336acefb163edaa7692f10cb4e46272e88
Olena Zdanovska <freshjs.ftm@gmail.com>
2024-06-17 10:53:38 +0300

    Refactor StopWatch

--- src/components/StopWatch/index.jsx
+++ src/components/StopWatch/index.jsx
@@ -7,43 +7,44 @@ class StopWatch extends Component {
       time: 0,
     };
     this.identificator = null;
-    // this.start(); // не можна в контрукторі викликати метод який встановлює новий стан!!!
-    console.log('constructor');
   }
+
+  tick = () => {
+    this.setState({ time: this.state.time + 1 });
+  };
   start = () => {
-    this.identificator = setInterval(() => {
-      this.setState({ time: this.state.time + 1 });
-    }, 1000);
+    if (this.identificator === null) {
+      this.identificator = setInterval(this.tick, 1000);
+    }
   };
   stop = () => {
     clearInterval(this.identificator);
     this.identificator = null;
-  }
-// викликається ОДИН раз після першого рендера!!!!
-  componentDidMount(){
-    this.start(); // місце для виклику функцій, які встановлюють новий стан при монтуванні компонента
-    console.log('componentDidMount');
-  }
-// викликається кожен раз після другого і більше рендера!!!!
-  componentDidUpdate(){
-    console.log('componentDidUpdate');
-  }
-  // викликається перед розмонтуванням(знищенням) компонента
-  componentWillUnmount(){
+  };
+  reset = () => {
     this.stop();
-    console.log('componentWillUnmount');
+    this.setState({ time: 0 });
+  };
+
+  componentDidMount() {
+    this.start();
+  }
+
+  componentDidUpdate() {}
+
+  componentWillUnmount() {
+    this.stop();
   }
 
   render() {
     const { time } = this.state;
-    // this.start();  // не можна викликати метод який встановлює новий стан через те, що це приводить до рекурсивного виклику!!!!
-    console.log('render');
+
     return (
       <article>
         <h2>{time}</h2>
         <button onClick={this.start}>start</button>
         <button onClick={this.stop}>stop</button>
-        <button>reset</button>
+        <button onClick={this.reset}>reset</button>
       </article>
     );
   }
