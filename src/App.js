bc2f2ea60d37bcd0825998775711da5e5c5a7ec6
Olena Zdanovska <freshjs.ftm@gmail.com>
2024-06-17 10:36:46 +0300

    Update App

--- src/App.js
+++ src/App.js
@@ -1,29 +1,12 @@
-import { Component } from 'react';
 import './App.css';
 import StopWatch from './components/StopWatch';
 
-class App extends Component {
-  constructor() {
-    super();
-    this.state = {
-      isVisible: true,
-    };
-  }
-  render() {
-    const { isVisible } = this.state;
-    return (
-      <>
-        <button
-          onClick={() => {
-            this.setState({ isVisible: !isVisible });
-          }}
-        >
-          switch {isVisible ? 'hidden' : 'visible'}
-        </button>
-        {isVisible && <StopWatch />}
-      </>
-    );
-  }
+function App() {
+  return (
+    <>
+      <StopWatch />
+    </>
+  );
 }
 
 export default App;
