b0471e6225b779b70ab2e8bf09b9497f9c027b1e
Olena Zdanovska <freshjs.ftm@gmail.com>
2024-06-21 09:42:03 +0300

    Use props with default value

--- src/components/Button/index.jsx
+++ src/components/Button/index.jsx
@@ -3,8 +3,7 @@ import PropTypes from 'prop-types';
 
 import './style.css';
 
-function Button(props) {
-  const { content, number } = props;
+function Button({ content='content', number=5}) {
   return (
     <button className="btn">
       {content} {number}
@@ -15,11 +14,6 @@ function Button(props) {
 Button.propTypes = {
   content: PropTypes.string.isRequired,
   number: PropTypes.number,
-}
-
-Button.defaultProps = {
-  content: 'content',
-  number: 5,
-}
+};
 
 export default Button;
