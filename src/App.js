52d7c39e9e057e996430caf905540bea282d91eb
Olena Zdanovska <freshjs.ftm@gmail.com>
2024-06-24 10:17:42 +0300

    Add template & mixin, @import

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import MyLink from './components/MyLink';
+import StopWatch from './components/StopWatch';
 
 function App() {
   return (
     <>
      <MyLink href='#' content='my link content'/>
+     <StopWatch />
     </>
   );
 }
