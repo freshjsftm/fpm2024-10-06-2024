d2371efa73a18d557644a3eebbfe67797131aa78
Olena Zdanovska <freshjs.ftm@gmail.com>
2024-06-24 11:02:48 +0300

    Add render prop children

--- src/App.js
+++ src/App.js
@@ -1,12 +1,27 @@
 import './App.css';
-import MyLink from './components/MyLink';
-import StopWatch from './components/StopWatch';
+
+function List(props) {
+  const { title, numbers, children } = props;
+  return (
+    <article>
+      <h2 title={title}>List {numbers}</h2>
+      <ul>{children}</ul>
+    </article>
+  );
+}
 
 function App() {
   return (
     <>
-     <MyLink href='#' content='my link content'/>
-     <StopWatch />
+      <List title="text for title" numbers={2}>
+        <li>Max</li>
+        <li>Anna</li>
+      </List>
+      <List title="text for title" numbers={3}>
+        <li>item 1</li>
+        <li>item 2</li>
+        <li>item 3</li>
+      </List>
     </>
   );
 }
