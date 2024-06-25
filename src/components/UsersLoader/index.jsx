c72593f3a645425d77d4e04a6685c7ebe4772213
Olena Zdanovska <freshjs.ftm@gmail.com>
2024-06-25 11:49:08 +0300

    Add pagination

--- src/components/UsersLoader/index.jsx
+++ src/components/UsersLoader/index.jsx
@@ -8,25 +8,56 @@ class UsersLoader extends Component {
       users: [],
       isPending: false,
       error: null,
+      currentPage: 1,
     };
   }
-  componentDidMount() {
+
+  load = () => {
+    const {currentPage} = this.state;
     this.setState({ isPending: true });
-    fetch('https://randomuser.me/api/?results=5')
+    fetch(`https://randomuser.me/api/?results=5&page=${currentPage}&seed=fpm2024-1`)
       .then((response) => response.json())
       .then((data) => {
+        if (data.error) {
+          throw new Error(data.error);
+        }
         this.setState({ users: data.results });
       })
       .catch((error) => {
         this.setState({ error: error });
       })
-      .finally(()=>{
+      .finally(() => {
         this.setState({ isPending: false });
       });
   }
+
+  componentDidMount() {
+    this.load();
+  }
+
+  componentDidUpdate(prevProps, prevState){
+    if(prevState.currentPage !== this.state.currentPage){
+      this.load();
+    }
+  }
+
   showUsers = (user) => <li key={user.login.uuid}>{user.name.first}</li>;
+  prevPage = () => {
+    this.setState((state, props) => {
+      const { currentPage } = state;
+      if (currentPage > 1) {
+        return { currentPage: currentPage - 1 };
+      }
+    });
+  };
+  nextPage = () => {
+    this.setState((state, props) => {
+      const { currentPage } = state;
+      return { currentPage: currentPage + 1 };
+    });
+  };
   render() {
-    const { users, isPending, error } = this.state;
+    const { users, isPending, error, currentPage } = this.state;
     if (isPending) {
       return <div>Loading...</div>;
     }
@@ -36,6 +67,11 @@ class UsersLoader extends Component {
     return (
       <section>
         <h2>Users:</h2>
+        <div>
+          <button onClick={this.prevPage}>&lt; {/*&lt; - unicode */} </button>
+          <span> {currentPage} </span>
+          <button onClick={this.nextPage}>&gt;</button>
+        </div>
         {users.length ? (
           <ul>{users.map(this.showUsers)}</ul>
         ) : (
