bf18c9d743d1f2cd6761efed91e64922039501d3
Olena Zdanovska <freshjs.ftm@gmail.com>
2024-07-01 08:46:34 +0300

    Add react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,11 +1,27 @@
 import './App.css';
+import { BrowserRouter, Routes, Route, NavLink } from 'react-router-dom';
 import WindowWork from './components/WindowWork';
-
+import UsersLoader from './components/UsersLoader';
+import StopWatch from './components/StopWatch';
 
 function App() {
   return (
     <>
-     <WindowWork />
+      <BrowserRouter>
+      <nav>
+        <ul>
+          <li><NavLink to='/'>home</NavLink></li>
+          <li><NavLink to='/users'>users</NavLink></li>
+          <li><NavLink to='/stop-watch'>stop watch</NavLink></li>
+        </ul>
+      </nav>
+        <Routes>
+          <Route path="/" element={<WindowWork />}></Route>
+          <Route path="/users" element={<UsersLoader />}></Route>
+          <Route path="/stop-watch" element={<StopWatch />}></Route>
+        </Routes>
+        <footer>2024</footer>
+      </BrowserRouter>
     </>
   );
 }
